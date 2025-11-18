4c532613640249eb477edf7e61dfb7a37871ebdf
pouya <pouyakhaleghi82@gmail.com>
2025-11-18 17:09:41 +0330

    functional model

--- Script.js
+++ Script.js
@@ -4,19 +4,39 @@ const reset = document.querySelector('.reset')
 const counter_value = document.querySelector('.counter-value')
 
 
-let i = 0
+let i = Number(counter_value.textContent)
+//
+// plus.addEventListener('click', () => {
+//     i++
+//     document.querySelector('.counter-value').textContent = i
+// });
+//
+// minus.addEventListener('click', () => {
+//     i--
+//     document.querySelector('.counter-value').textContent = i
+// })
+//
+// reset.addEventListener('click', () => {
+//     i = 0
+//     document.querySelector('.counter-value').textContent = i
+// })
 
-plus.addEventListener('click', () => {
-    i++
-    document.querySelector('.counter-value').innerText = i
-});
 
-minus.addEventListener('click', () => {
-    i--
-    document.querySelector('.counter-value').innerText = i
-})
 
-reset.addEventListener('click', () => {
-    i = 0
-    document.querySelector('.counter-value').innerText = i
-})
+// بصورت فانکشنال
+
+function counter (e){
+    const classList = e.target.classList
+    if (classList.contains('plus')) i++;
+    else if (classList.contains('minus')) i--;
+    else i=0;
+
+    counter_value.textContent = i;
+
+}
+
+
+plus.addEventListener('click', counter)
+minus.addEventListener('click', counter)
+reset.addEventListener('click', counter)
+
